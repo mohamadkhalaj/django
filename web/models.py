bc3b921be385daa56194d91ef9065eb4e642077b
mohammad <mohammadkhalaj@outlook.com>
2020-08-01 18:08:50 +0430

    view page and rest for expense completed

--- web/models.py
+++ web/models.py
@@ -17,3 +17,9 @@ class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.text}\t{self.amount}\t{self.date}"
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __str__(self):
+        return f"{self.user} token"
--- web/views.py
+++ web/views.py
@@ -1,2 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from .models import Expense, Income, Token, User
+import datetime
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt
+def submit_expense(request):
+
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+
+    if 'date' not in request.POST:
+        date = datetime.datetime.now()
+    else:
+        date = request.POST['date']
+
+    Expense.objects.create(user = this_user, amount = request.POST['amount'], date = date, text = request.POST['text'])
+    return JsonResponse({
+        "status": 200
+     }, encoder=JSONEncoder)
