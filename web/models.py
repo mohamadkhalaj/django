423e41a93aa78ba99dbd070e1618877dfeacabdc
mohammad <mohammadkhalaj@outlook.com>
2020-08-01 16:39:23 +0430

    beautified

--- web/models.py
+++ web/models.py
@@ -7,10 +7,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user =  models.ForeignKey(User,on_delete=models.CASCADE)
-
+    def __str__(self):
+        return f"{self.text}\t{self.amount}\t{self.date}"
 
 class Income(models.Model):
     text = models.CharField(max_length=253)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.text}\t{self.amount}\t{self.date}"
