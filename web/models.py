6bf9845127b25c96e672d97dd9338c1e7bd17deb
mohammad <mohammadkhalaj@outlook.com>
2020-08-01 15:30:44 +0430

    income added and added time

--- web/models.py
+++ web/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Expense(models.Model):
     text = models.CharField(max_length=253)
-    date = models.DateField()
+    date = models.DateTimeField()
     amount = models.BigIntegerField()
     user =  models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=253)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
